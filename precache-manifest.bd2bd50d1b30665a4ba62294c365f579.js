863980f17b4a3d2fa992d334b164f5d2f726cb93
bdcoelho <ben_coelho@hotmail.com>
2020-09-18 08:52:35 +1000

    Updates

---
+++ precache-manifest.bd2bd50d1b30665a4ba62294c365f579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d933a406693faf4f3f7eb3bc24c1c8",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "ef519ec598e874d6c179",
+    "url": "/employee-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1b3a3166b8a5afd7b254",
+    "url": "/employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef519ec598e874d6c179",
+    "url": "/employee-directory/static/js/2.d449a78b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-directory/static/js/2.d449a78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3a3166b8a5afd7b254",
+    "url": "/employee-directory/static/js/main.b883d804.chunk.js"
+  },
+  {
+    "revision": "1e6bf1e4dbb706bdc440",
+    "url": "/employee-directory/static/js/runtime-main.a10c72b0.js"
+  }
+]);
